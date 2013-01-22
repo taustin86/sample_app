9805a6dac5f23909aeeeac2f9f737fa075d54dbe
Tamara Austin <taustin@wrldtouchtech.com>
2013-01-21 21:33:30 -0500

    Add test for email saved as downcase

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -77,6 +77,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email with mixed case" do
+    let(:mixed_case_email) { "FOO@BaR.com" }
+
+    it "should be saved as lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
