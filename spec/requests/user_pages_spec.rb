1fb578d32c27baa37970164dc6d481ba6b66a8b6
Tamara Austin <taustin@wrldtouchtech.com>
2013-01-23 16:15:08 -0500

    Allow users to sign in and out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -44,6 +44,8 @@ describe "UserPages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: "Welcome") }
+        it { should have_link('Sign out', href: signout_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
       end
     end
   end
