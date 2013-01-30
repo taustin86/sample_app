134b3418aa4402bc3810859984d95cb1af64cf93
Tamara Austin <taustin@wrldtouchtech.com>
2013-01-30 02:03:57 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -51,13 +52,6 @@ class UsersController < ApplicationController
 
   private
     
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def new_user
       redirect_to root_path if signed_in?
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  has_many :microposts, dependent: :destroy
   has_secure_password
 
   before_save { |user| user.email.downcase! }
@@ -24,6 +25,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }                    
   validates :password_confirmation, presence: true
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
 
   private
